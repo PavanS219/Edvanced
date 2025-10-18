57ddd714c4018cd2f92698619e9bc4a58d53107e
Pavan <147194372+PavanS219@users.noreply.github.com>
2025-10-18 13:05:45 +0530

    g

--- MAIN_CODE.py
+++ MAIN_CODE.py
@@ -5,8 +5,8 @@ import time
 import logging
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
-from langchain.chains.summarize import load_summarize_chain
-from langchain.chains.llm import LLMChain
+from langchain.chains import load_summarize_chain
+from langchain.chains import LLMChain
 from langchain_community.document_loaders import UnstructuredURLLoader
 from yt_dlp import YoutubeDL
 from langchain_core.documents import Document
